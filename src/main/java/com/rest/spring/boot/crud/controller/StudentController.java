2eed2d28aa57ef3acedc2b88e8b595fc0255ec73
nirav-dashtechinc <75743304+nirav-dashtechinc@users.noreply.github.com>
2021-05-06 11:39:00 -0400

    Update StudentController.java

--- src/main/java/com/rest/spring/boot/crud/controller/StudentController.java
+++ src/main/java/com/rest/spring/boot/crud/controller/StudentController.java
@@ -9,6 +9,7 @@ public class StudentController {
 
 	@GetMapping
 	public String getHome(ModelAndView mav) {
+		System.out.println("StudentController");
 		mav.addObject("welcome", "Welcome, User");
 		return "index";
 	}
